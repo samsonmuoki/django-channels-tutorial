797ec716059ae9dbcd867ed1bb2fbc4a30295530
sam <smuoki@healthcloud.co.ke>
2019-09-28 12:47:23 +0300

    Comment ASGI_APPLICATION temporarily

--- channels_tutorial/channels_tutorial/settings.py
+++ channels_tutorial/channels_tutorial/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'channels_tutorial.wsgi.application'
 
-ASGI_APPLICATION = "channels_tutorial.routing.application"
+#ASGI_APPLICATION = "channels_tutorial.routing.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
